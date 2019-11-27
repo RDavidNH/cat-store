30c1e7259ca168bc82df0d87cf7fec09a030255c
BeAnjara <beanjarahoussen@gmail.com>
2019-11-27 11:53:42 +0300

    Set routes to user show

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
+  resources :users, only: [:show]
   root "items#index"
 end
