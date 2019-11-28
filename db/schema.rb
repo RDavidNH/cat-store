fa514b2bd9f4bc18777faffc75844a4efbf33d37
Rabedaoro David N H <rdavidnh@gmail.com>
2019-11-29 02:07:30 +0300

    updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_070553) do
+ActiveRecord::Schema.define(version: 2019_11_28_185706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cart_items", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "cart_id"
-    t.integer "quantity", default: 1
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
-    t.index ["item_id"], name: "index_cart_items_on_item_id"
-  end
-
   create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -39,6 +32,16 @@ ActiveRecord::Schema.define(version: 2019_11_28_070553) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["item_id"], name: "index_orders_on_item_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,14 +50,11 @@ ActiveRecord::Schema.define(version: 2019_11_28_070553) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "cart_id"
-    t.string "firstname"
-    t.string "lastname"
-    t.text "description"
-    t.index ["cart_id"], name: "index_users_on_cart_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "users", "carts"
 end
