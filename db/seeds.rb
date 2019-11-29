99908c4e785b4af8c172bbd7cddcf2cbfd529ad1
BeAnjara <beanjarahoussen@gmail.com>
2019-11-29 11:13:08 +0300

    Change all picture format

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Item.destroy_all
 		# title: Faker::Lorem.sentence(word_count: 2),
 		description: Faker::Creature::Cat.breed,
 		price: Faker::Number.between(from: 15, to: 100),
-		image_url: " "
+		image_url: "chat#{index}.jpg"
 	)
 end
 
